fd43f91724e94d16cb4b0a12edb4514993d44958
Mary Angerman <retrokoks@gmail.com>
2023-03-06 13:21:26 +0300

    выполненые задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,32 +1,30 @@
 "use strict"
-const solveEquation = (a, b, c) => {
-  const D = b**-4*a*c;
-  if(a == 0) return false;
-  const rezult = {};
-  console.log("D = " + D);
-  if (D > 0) {
-    return array = [(-b + Math.sqrt(D)) / (2 * a),
-    (-b - Math.sqrt(D)) / (2 * a)];
-  } else if (D == 0){
-    return array = [-b / (2 * a)];
-  } else if (D < 0) {
-    return array = [];
-  }
- };
 
-//const testCase = (a, b, c) => console.log(solveEquation (a, b, c));
+const solveEquation = (a, b, c) => {
+  const discriminant = ((b ** 2) - 4 * a * c);
+  if (discriminant < 0) {
+    return array = [];
+  } else if (discriminant === 0) {
+    return array = [-b / (2 * a)];
+  } else {
+    return array = [(-b + Math.sqrt(discriminant)) / (2 * a),
+    (-b - Math.sqrt(discriminant)) / (2 * a)];
+  }
+}
+
+//const testCase = (a, b, c) => console.log(solveEquation(a, b, c));
 //testCase(15, 345, 6);
 
 const calculateTotalMortgage = (percent, constribution, amount, countMonths) => {
   if (isNaN(persen) === true ||
-  isNaN(constribution) === true ||
-  isNaN(amount) === true ||
-  isNaN(countMonths) === true)
-  return false;
+    isNaN(constribution) === true ||
+    isNaN(amount) === true ||
+    isNaN(countMonths) === true)
+    return false;
   else {
     percent = (percent / 100) / 12;
     const loanBody = (amount - constribution);
-    const payment = loanBody * (percent + (percent / (((1 + percent)**countMonths) - 1)));
+    const payment = loanBody * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
     const totalLoan = (payment * countMonths);
     return Number(totalLoan.toFixed(2));
   }
